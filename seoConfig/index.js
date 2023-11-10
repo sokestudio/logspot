f8f4df15dc14e433bc467a3d6e78b905f5854485
sokedev <2096183850@qq.com>
2023-11-10 22:18:40 +0800

    test1

--- seoConfig/index.js
+++ seoConfig/index.js
@@ -1,11 +1,11 @@
 export default {
   title: "Logspot - Opensource changelog template",
   description:
-    "Logspot is an open source change log template made with Nuxt, Vue, and Tailwindcss.",
+    "Logspot is an open source change log templat.",
   og: {
     title: "Logspot - Opensource changelog template",
     description:
-      "Logspot is an open source change log template made with Nuxt, Vue, and Tailwindcss.",
+      "Logspot is an opeuxt, Vue, and Tailwindcss.",
     image: "https://logspot.vercel.app/logspot-banner.png",
     url: "https://logspot.vercel.app",
   },
